7b14f06148c83bce8462025ee2898fd466312a16
wookoo <nanayagoon@daum.net>
2019-08-08 02:29:07 +0900

    달력 파랑 보라 나눠 표기

--- app/src/main/java/am/ze/wookoo/myapplication/CalanderActivity.java
+++ app/src/main/java/am/ze/wookoo/myapplication/CalanderActivity.java
@@ -8,9 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
 
 
 public class CalanderActivity extends AppCompatActivity {
@@ -40,11 +49,60 @@ public class CalanderActivity extends AppCompatActivity {
 
         mView = findViewById(R.id.calendarView);
 
-        mView.setSelectionColor(Color.parseColor("#B01717"));
-        mView.setSelectedDate(CalendarDay.from(2019,7,10));
-        mView.setDateSelected(CalendarDay.from(2019,7,15),true);
-        mView.setSelectionColor(Color.parseColor("#FFF000"));
-        mView.setDateSelected(CalendarDay.from(2019,7,16),true);
+
+
+
+        List<CalendarDay>  failList = new ArrayList<>();
+
+        List<CalendarDay> successLIst = new ArrayList<>();
+
+
+        failList.add(CalendarDay.from(CalendarDay.today().getYear(), CalendarDay.today().getMonth(), 1 ));
+        failList.add(CalendarDay.from(CalendarDay.today().getYear(), CalendarDay.today().getMonth(), 3 ));
+
+        for (int i = CalendarDay.today().getMonth()-1; i >= 0; i -- ){
+            for(int k = 1; k < 27; k++){
+                double ran = Math.random();
+                if (ran < 0.3){
+                    successLIst.add(CalendarDay.from(CalendarDay.today().getYear(),i,k));
+                }
+            }
+        }
+
+        for (int i = CalendarDay.today().getMonth(); i >= 0; i -- ){
+            for(int k = 1; k < CalendarDay.today().getDay(); k++){
+                if(k == 1 || k == 3){
+                    continue;
+                }
+                double ran = Math.random();
+                if (ran < 0.5){
+                    successLIst.add(CalendarDay.from(CalendarDay.today().getYear(),i,k));
+                }
+            }
+        }
+
+
+        //파란색 4F8BF6 보라색 B880F7
+        mView.addDecorator(new FailEventDecorator(Color.parseColor("#B880F7"),failList,CalanderActivity.this));
+
+        mView.addDecorator(new EventDecorator(Color.parseColor("#4F8BF6"),successLIst,CalanderActivity.this));
+
+        mView.setSelectionMode(MaterialCalendarView.SELECTION_MODE_NONE);
+
+        final LinearLayout mFail = findViewById(R.id.fail);
+
+        mView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                if(date.getMonth() != CalendarDay.today().getMonth()){
+                    mFail.setVisibility(View.INVISIBLE);
+                }else{
+                    mFail.setVisibility(View.VISIBLE);
+                }
+            //    Toast.makeText(CalanderActivity.this,"" + date.getMonth() + "/" + CalendarDay.today().getMonth(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 
--- app/src/main/java/am/ze/wookoo/myapplication/FailEventDecorator.java
+++ app/src/main/java/am/ze/wookoo/myapplication/FailEventDecorator.java
@@ -5,8 +5,6 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.text.style.ForegroundColorSpan;
 
-//import com.project.sample_calendar.R;
-import am.ze.wookoo.myapplication.R;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
@@ -15,17 +13,19 @@ import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 import java.util.Collection;
 import java.util.HashSet;
 
+//import com.project.sample_calendar.R;
+
 /**
  * Decorate several days with a dot
  */
-public class EventDecorator implements DayViewDecorator {
+public class FailEventDecorator implements DayViewDecorator {
 
     private final Drawable drawable;
     private int color;
     private HashSet<CalendarDay> dates;
 
-    public EventDecorator(int color, Collection<CalendarDay> dates,Activity context) {
-        drawable = context.getResources().getDrawable(R.drawable.blue_mark);
+    public FailEventDecorator(int color, Collection<CalendarDay> dates, Activity context) {
+        drawable = context.getResources().getDrawable(R.drawable.pass_learn);
         this.color = color;
         this.dates = new HashSet<>(dates);
     }
@@ -38,7 +38,7 @@ public class EventDecorator implements DayViewDecorator {
     @Override
     public void decorate(DayViewFacade view) {
         view.setSelectionDrawable(drawable);
-        //view.addSpan(new DotSpan(5, color)); // 날자밑에 점
+       // view.addSpan(new DotSpan(5, color)); // 날자밑에 점
         view.addSpan(new ForegroundColorSpan(Color.WHITE));
     }
 }
